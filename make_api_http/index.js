96a311e694ba904a7eae0db19bc263bf44a47f34
Mayur Malaviya <mmalaviya517@gmail.com>
2025-03-25 00:43:15 +0530

    File System Module Practice

--- make_api_http/index.js
+++ make_api_http/index.js
@@ -22,30 +22,71 @@
  * 
 */
 
-const http = require('http');
-const port = 5500;
-const host = "localhost";
-const {arr} = require('./data.js')
-function _CS(fn, value) {
-    return http.createServer(fn).listen(port , host , (error)=>{
-        console.log(`====>> server is runnning ...`,)
-    })
-}
+// const http = require('http');
+// const port = 5500;
+// const host = "localhost";
+// const {arr} = require('./data.js')
+// function _CS(fn, value) {
+//     return http.createServer(fn).listen(port , host , (error)=>{
+//         console.log(`====>> server is runnning ...`,)
+//     })
+// }
 
-let homePage = (req, res) => {
-    let { url } = req
-    if (url === "/get-post") {
-        res.writeHead(200,{ 'Content-Type':'application/json'})
-        // res.write(JSON.stringify({name:"mayur"}))
-        res.write(JSON.stringify(arr))
-        res.end()
-    } else if (url === '/create-post') {
-        res.end("create")
-    } else if (url === 'update-post') {
-        res.end('update')
-    } else if (url === 'delete-post') {
-        res.end('delete')
+// let homePage = (req, res) => {
+//     let { url } = req
+//     if (url === "/get-post") {
+//         res.writeHead(200,{ 'Content-Type':'application/json'})
+//         // res.write(JSON.stringify({name:"mayur"}))
+//         res.write(JSON.stringify(arr))
+//         res.end()
+//     } else if (url === '/create-post') {
+//         res.end("create")
+//     } else if (url === 'update-post') {
+//         res.end('update')
+//     } else if (url === 'delete-post') {
+//         res.end('delete')
+//     }
+// }
+
+// _CS(homePage);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const http = require('http');
+const port = 8000
+function handler(req, res) {
+    switch (req.url) {
+        case "/":
+            res.end("This is home page!!");
+            break;
+        case "/get" :
+            res.end(' This is get page');
+            break;
+        default:
+            res.end("404  page not found!!")
+            break;
     }
 }
 
-_CS(homePage);
+function _SERVER({handler, port}) {
+    return http.createServer(handler).listen(port, (err, msg)=> {
+        console.log(`Server running on port ${port} ===>>> `)
+    });
+}
+
+
+_SERVER({handler, port})
+
+
